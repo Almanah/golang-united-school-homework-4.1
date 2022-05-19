1ec1267b486a68f6b088575e644701ce91395e7f
Denis Savchyshkin <Denis_Savchyshkin@epam.com>
2022-05-19 15:10:11 +0300

    fixed test

--- reverse_string.go
+++ reverse_string.go
@@ -2,10 +2,9 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	runes := []rune(input)
-	result_runes := make([]rune, len(runes))
+
 	for i := len(runes) - 1; i >= 0; i-- {
-		result_runes = append(result_runes, runes[i])
+		output += string(runes[i])
 	}
-	output = string(result_runes)
 	return output
 }
